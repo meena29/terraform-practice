603c273b188be72a5095eed79e086eaf900171b7
Meena Brahmajosyula <meena.brahmajosyula@gmail.com>
2025-01-22 13:39:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "Meenajan2025"
+    bucket         = "meena6665522jan2025"
     key            = "terraform-infra-cicd/terraform.tfstate"
     region         = "ca-central-1"
     dynamodb_table = "terraform-lock-table"
